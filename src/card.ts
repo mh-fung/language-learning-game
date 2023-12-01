5f5152deb70c3d3151c014ac86ed1f8b909c8d78
Hei Fung <heifung@Heis-MacBook-Pro.local>
2023-12-01 17:05:33 +0000

    Defined the basic structure of the layout, added ts to render card content

---
+++ src/card.ts
@@ -0,0 +1,21 @@
+//type alias of a card
+export type Card = {
+    chineseWord: string;
+    englishMeaning: string;    
+};
+
+
+export const cardArray: Card[] = [
+    {chineseWord: "一", englishMeaning: "One"}, 
+    {chineseWord: "一", englishMeaning: "One"}, 
+    {chineseWord: "一", englishMeaning: "One"}, 
+    {chineseWord: "一", englishMeaning: "One"}, 
+    {chineseWord: "一", englishMeaning: "One"}, 
+    {chineseWord: "一", englishMeaning: "One"}, 
+    {chineseWord: "一", englishMeaning: "One"}, 
+    {chineseWord: "一", englishMeaning: "One"}, 
+    {chineseWord: "一", englishMeaning: "One"}, 
+    {chineseWord: "一", englishMeaning: "One"}, 
+    {chineseWord: "一", englishMeaning: "One"}, 
+    {chineseWord: "一", englishMeaning: "One"}, 
+];
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
 import './styles/main.scss';
+import { Card, cardArray } from './card';
+
+//Gaining access to the html elements
+const cardContainer = document.querySelector<HTMLDivElement>(".card-container")
+const buttonStart = document.querySelector<HTMLButtonElement>(".button");
+if (!cardContainer || !buttonStart) {
+  throw new Error("Issues with Selector");
+}
+
+//Function that renders the card content. 
+const renderCardContent = (cardArray: Card[]): any => {
+  cardArray.forEach(card => {
+    cardContainer.innerHTML += `<div class="card">${card.chineseWord}, ${card.englishMeaning}</div>`
+  });
+};
+
+//Add event listener to the button
+buttonStart.addEventListener("click", renderCardContent(cardArray));
