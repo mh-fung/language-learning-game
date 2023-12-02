89456c8f8855ff2c90bf9f8df05a3754c11c3a9e
Hei Fung <heifung@Heis-MacBook-Pro.local>
2023-12-02 15:46:29 +0000

    Added renderCardsContent function and clear content function

--- src/card.ts
+++ src/card.ts
@@ -4,18 +4,32 @@ export type Card = {
     englishMeaning: string;    
 };
 
+export const cardArray1: Card[] = [
+    {chineseWord: "一", englishMeaning: "One"}, 
+    {chineseWord: "女", englishMeaning: "Female"}, 
+    {chineseWord: "火", englishMeaning: "Fire"}, 
+    {chineseWord: "心", englishMeaning: "Heart"}, 
+    {chineseWord: "男", englishMeaning: "Male"}, 
+    {chineseWord: "女", englishMeaning: "Earth"}, 
+    {chineseWord: "男", englishMeaning: "Male"}, 
+    {chineseWord: "一", englishMeaning: "One"}, 
+    {chineseWord: "火", englishMeaning: "Fire"}, 
+    {chineseWord: "田", englishMeaning: "Field"}, 
+    {chineseWord: "田", englishMeaning: "Field"}, 
+    {chineseWord: "心", englishMeaning: "Heart"}, 
+];
 
-export const cardArray: Card[] = [
+export const cardArray2: Card[] = [
+    {chineseWord: "二", englishMeaning: "One"}, 
+    {chineseWord: "女", englishMeaning: "Female"}, 
+    {chineseWord: "火", englishMeaning: "Fire"}, 
+    {chineseWord: "心", englishMeaning: "Heart"}, 
+    {chineseWord: "男", englishMeaning: "Male"}, 
+    {chineseWord: "女", englishMeaning: "Earth"}, 
+    {chineseWord: "男", englishMeaning: "Male"}, 
     {chineseWord: "一", englishMeaning: "One"}, 
-    {chineseWord: "一", englishMeaning: "One"}, 
-    {chineseWord: "一", englishMeaning: "One"}, 
-    {chineseWord: "一", englishMeaning: "One"}, 
-    {chineseWord: "一", englishMeaning: "One"}, 
-    {chineseWord: "一", englishMeaning: "One"}, 
-    {chineseWord: "一", englishMeaning: "One"}, 
-    {chineseWord: "一", englishMeaning: "One"}, 
-    {chineseWord: "一", englishMeaning: "One"}, 
-    {chineseWord: "一", englishMeaning: "One"}, 
-    {chineseWord: "一", englishMeaning: "One"}, 
-    {chineseWord: "一", englishMeaning: "One"}, 
+    {chineseWord: "火", englishMeaning: "Fire"}, 
+    {chineseWord: "田", englishMeaning: "Field"}, 
+    {chineseWord: "田", englishMeaning: "Field"}, 
+    {chineseWord: "心", englishMeaning: "Heart"}, 
 ];
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,34 @@
 import './styles/main.scss';
-import { Card, cardArray } from './card';
+import { Card, cardArray1 } from './card';
 
 //Gaining access to the html elements
-const cardContainer = document.querySelector<HTMLDivElement>(".card-container")
+const cardContainer = document.querySelector<HTMLDivElement>(".card-container");
 const buttonStart = document.querySelector<HTMLButtonElement>(".button");
-if (!cardContainer || !buttonStart) {
+const buttonClear = document.querySelector<HTMLButtonElement>(".button--clear");
+if (!cardContainer || !buttonStart|| !buttonClear) {
   throw new Error("Issues with Selector");
-}
-
-//Function that renders the card content. 
-const renderCardContent = (cardArray: Card[]): any => {
-  cardArray.forEach(card => {
-    cardContainer.innerHTML += `<div class="card">${card.chineseWord}, ${card.englishMeaning}</div>`
-  });
 };
 
+const cards = document.querySelectorAll<HTMLDivElement>(".card");
+
+
+//Function that renders the card content. for card[0] -> card--1
+const renderCardContent = (cardArray: Card[]): any => {
+  cards.forEach(card => {
+    card.innerHTML = `<p>${cardArray[Number(card.innerText) - 1].chineseWord}</p><p>${cardArray[Number(card.innerText) - 1].englishMeaning}</p>`
+  });
+  console.log("here")
+};
+
+
 //Add event listener to the button
-buttonStart.addEventListener("click", renderCardContent(cardArray));
+buttonStart.addEventListener("click", renderCardContent(cardArray1));
+
+//function to clear the content
+const flipOver = () => {
+  cards.forEach(card => {
+    card.innerHTML = ""
+  })
+}
+//Add event listener to the button
+buttonClear.addEventListener("click", flipOver)
