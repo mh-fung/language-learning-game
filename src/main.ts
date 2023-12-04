e2993cdc1c4241b5e4df2cdf707b7e2f022a6cb6
Hei Fung <heifung@Heis-MacBook-Pro.local>
2023-12-04 15:46:08 +0000

    Added ts for flipping card

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,23 @@ import { Card, cardArray1 } from './card';
 //Gaining access to the html elements
 const cardContainer = document.querySelector<HTMLDivElement>(".card-container");
 const buttonStart = document.querySelector<HTMLButtonElement>(".button");
+const buttonBegin = document.querySelector<HTMLButtonElement>(".buttonB");
 const buttonClear = document.querySelector<HTMLButtonElement>(".button--clear");
-if (!cardContainer || !buttonStart|| !buttonClear) {
+if (!cardContainer || !buttonBegin|| !buttonStart|| !buttonClear) {
   throw new Error("Issues with Selector");
 };
 
 const cards = document.querySelectorAll<HTMLDivElement>(".card");
 
 
+
+
+//function that flips all cards
+const flipCards = () => {
+  cards.forEach(card => {
+    card.classList.toggle("flip");
+  })
+}
 //Function that renders the card content. for card[0] -> card--1
 const renderCardContent = (cardArray: Card[]): any => {
   cards.forEach(card => {
@@ -19,10 +28,10 @@ const renderCardContent = (cardArray: Card[]): any => {
   });
   console.log("here")
 };
-
+buttonStart.addEventListener("click", renderCardContent(cardArray1))
 
 //Add event listener to the button
-buttonStart.addEventListener("click", renderCardContent(cardArray1));
+buttonBegin.addEventListener("click", flipCards);
 
 //function to clear the content
 const flipOver = () => {
@@ -32,3 +41,33 @@ const flipOver = () => {
 }
 //Add event listener to the button
 buttonClear.addEventListener("click", flipOver)
+
+
+//Game begins
+let firstCard: string;
+let secondCard: string;
+let hasFlippedCard = false;
+
+const flipCard = (event: Event) => {
+  const target = event.currentTarget as HTMLDivElement;
+  target.classList.toggle("flip");
+  if (hasFlippedCard === false) {
+    firstCard = target.innerHTML;
+    hasFlippedCard = true;
+  } else {
+    secondCard = target.innerHTML
+  }
+}
+const checkResult = ()=> {
+  if (firstCard === secondCard) {
+    console.log("Match!")
+  } else if (firstCard !== secondCard){
+    console.log("sorry")
+    firstCard = ""
+  }
+}
+
+
+cards.forEach(card => {
+  card.addEventListener("click", flipCard)
+})
